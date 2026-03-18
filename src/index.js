32f81b515fb2ead9600d5ba050becd0859b3096f
Your Name <you@example.com>
2026-03-18 15:50:04 -0300

    fix: retirar html

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
 // src/index.js
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-V5KBS6PRNL"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'G-V5KBS6PRNL');
-</script>
 import express from "express";
 import axios from "axios";
 import { getEventos } from "./ga4.js";
