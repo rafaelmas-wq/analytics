f08e7d8c68564a840852d156b7a703a22a953d18
Your Name <you@example.com>
2026-03-18 15:44:43 -0300

    install ga4 tag

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 // src/index.js
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-V5KBS6PRNL"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-V5KBS6PRNL');
+</script>
 import express from "express";
 import axios from "axios";
 import { getEventos } from "./ga4.js";
