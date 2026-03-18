abf4b40bdd6a158e99fbd7bfd4e152a4cf0c0018
Your Name <you@example.com>
2026-03-17 22:52:07 -0300

    fix: proprety id

--- src/index.js
+++ src/index.js
@@ -11,3 +11,20 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
+import { getEventos } from "./ga4.js";
+
+app.get("/ga4-test", async (req, res) => {
+  try {
+    const data = await getEventos(
+      "261098144",
+      "2021-01-01",
+      "2023-12-31",
+      ["page_view"]
+    );
+
+    res.json(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Erro ao buscar GA4");
+  }
+});
